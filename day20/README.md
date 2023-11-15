b09001533fe54bef58994f90f078cf190af374f7
Sarvottam999 <sarvottamsharma999@gmail.com>
2023-11-15 22:30:48 +0530

    day 20

---
+++ day20/README.md
@@ -0,0 +1,12 @@
+ 
+#  #Day20 of  #30DaysOfCode  ˗ˏˋ 📚 ☕︎ 🎧 ⚯ 💻 ˎˊ˗ 
+![project work video](./img/img.png)
+
+
+
+ 
+
+
+## Code: [click to see code for APP](/day20/code/app/) 📋
+ 
+ 
---
+++ day20/code/app/lib/main.dart
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() => runApp(CounterApp());
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider(
+        create: (_) => CounterCubit(),
+        child: CounterPage(),
+      ),
+    );
+  }
+}
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter')),
+      body: BlocBuilder<CounterCubit, int>(
+builder: (context, count) => Center(child: Text('Sarvottam' , style: TextStyle(fontSize: 20+count.toDouble()),)),
+        // builder: (context, count) => Text('$count', style: TextStyle(fontSize: 30),),
+      
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () => context.read<CounterCubit>().increment(),
+          ),
+          const SizedBox(height: 4),
+          FloatingActionButton(
+            child: const Icon(Icons.remove),
+            onPressed: () => context.read<CounterCubit>().decrement(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+ class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() => emit(state + 10);
+  void decrement() => emit(state - 10);
+
+ 
+}
